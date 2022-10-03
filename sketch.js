a754163187bb7c67ee7c8e50cba2fc7ecd0bb670
PedroMoller <103603537+PedroMoller@users.noreply.github.com>
2022-10-03 15:46:27 -0300

    03/10/2022

--- sketch.js
+++ sketch.js
@@ -7,35 +7,71 @@ var naveX = 0;
 var naveC;
 var naveD;
 var naveE;
+var pouso;
+var pousoI;
 
 function preload(){
     fundo = loadImage("bg.png");
-    naveI = loadImage("b_thrust_1.png");
+    naveI = loadAnimation("b_thrust_1.png");
+    naveC = loadAnimation("b_thrust_1.png","b_thrust_2.png","b_thrust_3.png");
+    naveD = loadAnimation("left_thruster_1.png","left_thruster_2.png");
+    naveE = loadAnimation("right_thruster_1.png","right_thruster_2.png");
+    naveC.looping = false;
+    naveD.looping = false;
+    naveE.looping = false;
+    pousoI = loadImage("lz.png");
 }
 
 function setup() {
     createCanvas(650,350);
+    pouso = createSprite(530,240);
+    pouso.addImage(pousoI);
+    pouso.scale = 0.4;
+    pouso.debug = false;
+    pouso.setCollider("rectangle",0,200,320,100);
     nave = createSprite(50,50);
-    nave.addImage(naveI);
+    nave.addAnimation("normal",naveI);
     nave.scale = 0.1;
+    nave.addAnimation("cima",naveC);
+    nave.addAnimation("direita",naveD);
+    nave.addAnimation("esquerda",naveE);
+   
 }
 
 function draw() {
     background(fundo);
+    //console.log(mouseX);
+    //console.log(mouseY);
     fill("#8A2BE2");
     text("VX:"+round(naveX),20,30);
     text("VY:"+round(naveY),20,45);
     drawSprites();
     naveY+=naveG;
     nave.position.y+=naveY;
-    if(keyDown("w")){
-        naveY-=0.5;
-    }
+
     nave.position.x+=naveX;
-    if(keyDown("a")){
+    if(nave.collide(pouso)){
+        nave.velocityX = 0;
+        nave.velocityY = 0;
+        naveX = 0;
+        naveY = 0;
+        nave.changeAnimation("normal");
+        textSize(17);
+        text("Parabens! Você ganhou!!",275,150);
+    }
+}
+
+function keyPressed(){
+    if(keyCode===87){
+        nave.changeAnimation("cima");
+        naveY=-3;
+    }
+    if(keyCode===65){
+        nave.changeAnimation("esquerda");
         naveX-=0.3;
     }
-    if(keyDown("d")){
+    if(keyCode===68){
+        nave.changeAnimation("direita");
         naveX+=0.3;
     }
 }
