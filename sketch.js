f9bcff34e5e689e3b8aff18abe8ca625f8cb338e
PedroMoller <103603537+PedroMoller@users.noreply.github.com>
2022-10-10 15:58:23 -0300

    10/10/2022

--- sketch.js
+++ sketch.js
@@ -14,6 +14,15 @@ var obstaculoI;
 var bum;
 var combustivel = 100;
 var stadoG = "jogando";
+var andar;
+var explosao;
+var gameover;
+var victoria;
+var morreu = false;
+var semCombustivel = false;
+var venceu = false;
+var reset;
+var resetI;
 
 function preload(){
     fundo = loadImage("bg.png");
@@ -28,6 +37,11 @@ function preload(){
     obstaculoI = loadImage("obstacle.png");
     bum = loadAnimation("crash2.png","crash3.png");
     bum.looping = false;
+    andar = loadSound("andar.wav");
+    explosao = loadSound("explosao.ogg");
+    gameover = loadSound("game over.wav");
+    victoria = loadSound("victoria.wav");
+    resetI = loadImage("reset.png");
 }
 
 function setup() {
@@ -49,6 +63,9 @@ function setup() {
     obstaculo.scale = 0.5;
     obstaculo.debug = false;
     obstaculo.setCollider("rectangle",-50,180,150,300);
+    reset = createSprite(325,175);
+    reset.addImage(resetI);
+    reset.scale = 0.5;
 }
 
 function draw() {
@@ -61,18 +78,29 @@ function draw() {
     text("C:"+round(combustivel),20,60);
     drawSprites();
     if(stadoG==="jogando"){
+        reset.visible = false;
         naveY+=naveG;
         nave.position.y+=naveY;
         nave.position.x+=naveX;
         if(nave.collide(pouso)){
+            if(!venceu&&!victoria.isPlaying()){
+                victoria.play();
+                venceu = true;
+            }
             stadoG = "venceu";
          
     }
         if(nave.collide(obstaculo)){
+            if(!morreu&&!explosao.isPlaying()){
+                explosao.play();
+                explosao.setVolume(0.3);
+                morreu = true;
+            }
             stadoG = "perdeu";
     }
     }
     if(stadoG==="venceu"){
+        reset.visible = true;
         nave.velocityX = 0;
         nave.velocityY = 0;
         naveX = 0;
@@ -80,15 +108,49 @@ function draw() {
         nave.changeAnimation("normal");
         textSize(17);
         text("Parabens! Você ganhou!!",275,150);
+        if(mousePressedOver(reset)){
+            stadoG = "jogando";
+            nave.x = 50;
+            nave.y = 50;
+            combustivel = 100;
+            nave.changeAnimation("normal");
+        }
     }
     if(stadoG==="perdeu"){
+        reset.visible = true;
         nave.velocityX = 0;
         nave.velocityY = 0;
         naveX = 0;
         naveY = 0;
         nave.changeAnimation("bum");
         text("Sua nave explodiu!",275,150);
+        if(mousePressedOver(reset)){
+            stadoG = "jogando";
+            nave.x = 50;
+            nave.y = 50;
+            combustivel = 100;
+            nave.changeAnimation("normal");
+        }
     }
+    if(combustivel<=0){
+        reset.visible = true;
+        if(!semCombustivel&&!gameover.isPlaying()){
+            gameover.play();
+            semCombustivel = true;
+        }
+        if(mousePressedOver(reset)){
+            stadoG = "jogando";
+            nave.x = 50;
+            nave.y = 50;
+            combustivel = 100;
+            nave.changeAnimation("normal");
+            nave.velocityX = 0;
+            nave.velocityY = 0;
+            naveX = 0;
+            naveY = 0;
+        }
+    }
+    
 }
 
 function keyPressed(){
@@ -96,15 +158,21 @@ function keyPressed(){
         nave.changeAnimation("cima");
         naveY=-3;
         combustivel-=5;
+        andar.play();
+        andar.setVolume(0.5)
     }
     if(keyCode===65&&combustivel>0&&stadoG==="jogando"){
         nave.changeAnimation("esquerda");
         naveX-=0.3;
         combustivel-=3;
+        andar.play();
+        andar.setVolume(0.5)
     }
     if(keyCode===68&&combustivel>0&&stadoG==="jogando"){
         nave.changeAnimation("direita");
         naveX+=0.3;
         combustivel-=3;
+        andar.play();
+        andar.setVolume(0.5)
     }
 }
